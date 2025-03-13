28534ecf10e96596a3f963c523845abbf2fe2d2e
Dinesh Maurya <dineshmourya02@gmail.com>
2025-03-13 12:35:25 +0530

    bgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import 'dotenv/config';
+ 
 
 const App = () => {
   const [coupon, setCoupon] = useState(null);
